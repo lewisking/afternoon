511c18c910a59f1f05c91716f24b4c0001f4a31f
Lewis King <519069+lewisking@users.noreply.github.com>
2025-11-21 09:43:54 +0700

    add 404

---
+++ 404.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+<div class="relative">
+  <?php get_template_part('template-parts/navigation') ?>
+
+  <div class="pt-[120px]">
+    <div class="max-w-screen-xl mx-auto px-5 md:px-8 lg:px-10">
+      <main>
+          <h1 class="text-[32px]/[38px] md:text-[48px]/[60px] lg:text-[52px]/[60px] text-brand-black font-bold tracking-tighter-4xl mb-4">
+            404: Page not found.
+          </h1>
+      </main>
+    </div>
+  </div>
+
+  <?php get_template_part('template-parts/footer') ?>
+
+</div>
+
+<?php get_footer(); ?>
